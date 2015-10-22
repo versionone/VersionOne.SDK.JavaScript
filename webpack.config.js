83e8bdee6b6d37fa9f52a95c5fe350cfa0a927cb
Andrew Smith <andrew.smith@versionone.com>
2015-10-22 15:51:34 -0400

    Correct location of resolves in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,11 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			//{
-			//	test: /\.(js)$/,
-			//	exclude: /node_modules/,
-			//	loader: 'babel-loader'
-			//},
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
 			{
 				test: /\.(coffee)$/,
 				exclude: /node_modules/,
